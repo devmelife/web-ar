4d867861b15764a168ec1f72894b1d5e8741966d
rheamaetorres <rhejadtor16@gmail.com>
2022-11-13 10:04:18 +0800

    script

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 window.onload = function () {
-    AFRAME.registerComponent("videohandler", {
-      init: function () {
-        var marker = this.el;
-  
-        this.vid = document.querySelector("#vid1");
-  
-        marker.addEventListener(
-          "markerFound",
-          function () {
-            this.vid.play();
-          }.bind(this)
-        );
-  
-        marker.addEventListener(
-          "markerLost",
-          function () {
-            this.vid.pause();
-            this.vid.currentTime = 0;
-          }.bind(this)
-        );
-      },
-    });
-  };
- 
-  
+  AFRAME.registerComponent("videohandler", {
+    init: function () {
+      var marker = this.el;
+
+      this.vid = document.querySelector("#vid1");
+
+      marker.addEventListener(
+        "markerFound",
+        function () {
+          this.vid.play();
+        }.bind(this)
+      );
+
+      marker.addEventListener(
+        "markerLost",
+        function () {
+          this.vid.pause();
+          this.vid.currentTime = 0;
+        }.bind(this)
+      );
+    },
+  });
+};
